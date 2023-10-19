78c5bfb7efe3ca142ccc962e62b3bbd3e341f49c
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-10-19 13:36:48 +0600

    feat: Added Footer with company info and social links

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import About from "../../components/About/About";
+import Footer from "../../components/Footer/Footer";
 import Banner from "../../components/Header/Banner/Banner";
 import Products from "../../components/Products/Products";
 
@@ -8,6 +9,7 @@ const Home = () => {
       <Banner></Banner>
       <Products></Products>
       <About></About>
+      <Footer></Footer>
     </div>
   );
 };
